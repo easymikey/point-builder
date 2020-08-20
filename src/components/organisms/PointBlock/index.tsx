68185908ae1e7d116a22c7f654f452fb8c1f0b6c
Mike Avdeev <avdeev2033@icloud.com>
2020-08-20 18:07:35 +0300

    create PointBlock

---
+++ src/components/organisms/PointBlock/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import AddPointForm from '../../moleculas/AddPointForm';
+import PointList from '../../moleculas/PointList';
+import PointListItem from '../../atoms/PointListItem';
+
+const List = () => (
+  <div>
+    <AddPointForm
+      inputRef={React.useRef()}
+      value=""
+      onChange={(event) =>
+        console.log('change', event)
+      }
+      onSubmit={(event) =>
+        console.log('change', event)
+      }
+    />
+    <PointList>
+      <PointListItem name="point1" />
+      <PointListItem name="point2" />
+      <PointListItem name="point3" />
+      <PointListItem name="point4" />
+    </PointList>
+  </div>
+);
+
+export default List;
