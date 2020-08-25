c5206081db254890c6fa1030b5c85ef44422dd74
Mike Avdeev <avdeev2033@icloud.com>
2020-08-25 13:37:02 +0300

    update button layout

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,41 +1,60 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
 
-const StyledButton = styled.button<ButtonType>`
+const StyledButton = styled.button`
   width: 150px;
   border: 2px;
-  border-top-right-radius: 0.5rem;
-  border-bottom-right-radius: 0.5rem;
+  font-size: 1.2rem;
+  border-radius: 0.2rem;
+  transition: 0.2s;
 
   display: flex;
   justify-content: center;
   align-items: center;
   border-style: solid;
 
-  background-color: #53a7ed;
-  border-color: #53a7ed;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  background-color: ${({ color }) =>
+    color === 'primary' ? '#53a7ed' : '#ed5653'};
+  border-color: ${({ color }) =>
+    color === 'primary' ? '#53a7ed' : '#ed5653'};
   color: white;
   outline: none;
+
+  &:hover {
+    border-color: ${({ color }) =>
+      color === 'primary' ? '#2d90e0' : ' #eb4744'};
+    background-color: ${({ color }) =>
+      color === 'primary' ? '#2d90e0' : '#eb4744'};
+  }
+
+  &:focus {
+    outline: none;
+    border-color: ${({ color }) =>
+      color === 'primary' ? '#2d90e0' : ' #eb4744'};
+    background-color: ${({ color }) =>
+      color === 'primary' ? '#2d90e0' : '#eb4744'};
+    box-shadow: 0 0 0 2px hsla(220, 90%, 56%, 0.2);
+  }
+
+  &:active {
+    transform: translateX(4px);
+  }
 `;
 
-
-
-interface ButtonType {
+interface ButtonProps {
+  color?: 'primary' | 'secondary';
   type: 'button' | 'submit';
   onClick: (event: any) => void;
-}
-
-interface ButtonProps extends ButtonType {
   buttonName: string;
 }
 
 const Button: FC<ButtonProps> = ({
+  color = 'primary',
   type = 'submit',
   onClick,
   buttonName,
 }) => (
-  <StyledButton type={type} onClick={onClick}>
+  <StyledButton type={type} onClick={onClick} color={color}>
     {buttonName}
   </StyledButton>
 );
