922ea696f52c78a29e4babab82b24cfac0ce3531
Mike Avdeev <avdeev2033@icloud.com>
2020-08-25 18:23:34 +0300

    transfet login responding on view geopoints

--- src/components/organisms/Map/index.tsx
+++ src/components/organisms/Map/index.tsx
@@ -1,51 +1,51 @@
-import React from 'react';
+import React, { FC } from 'react';
 import {
   YMaps,
   Map,
-  Placemark,
   FullscreenControl,
   ZoomControl,
   Polyline,
 } from 'react-yandex-maps';
+import GeoPointsOnMap, {
+  GeoPointsOnMapProps,
+} from '../../moleculas/GeoPointsOnMap';
 
-const MyMap = () => (
+const mapState = {
+  center: [55.76, 37.64],
+  zoom: 9,
+  controls: [],
+};
+
+const polylineOptions = {
+  balloonCloseButton: false,
+  strokeColor: '#000',
+  strokeWidth: 4,
+  strokeOpacity: 0.5,
+};
+
+const zoomControlOptions = {
+  float: 'right',
+  size: 'auto',
+};
+
+interface MapProps extends GeoPointsOnMapProps {
+  geometry: (number[] | never)[];
+}
+
+const MyMap: FC<MapProps> = ({
+  pointList,
+  geometry,
+  handleGeometryChange,
+}) => (
   <YMaps>
-    <Map
-      defaultState={{
-        center: [55.75, 37.57],
-        zoom: 9,
-      }}
-      width={400}
-      height={400}
-    >
-      <Placemark
-        geometry={[55.684758, 37.738521]}
-        properties={{
-          iconCaption: 'точка 1',
-        }}
-        options={{ draggable: true }}
+    <Map defaultState={mapState} width={400} height={400}>
+      <GeoPointsOnMap
+        pointList={pointList}
+        handleGeometryChange={handleGeometryChange}
       />
-      <Polyline
-        geometry={[
-          [55.684758, 37.738521],
-          [55.8, 37.4],
-          [55.7, 37.5],
-          [55.7, 37.4],
-        ]}
-        options={{
-          balloonCloseButton: false,
-          strokeColor: '#000',
-          strokeWidth: 4,
-          strokeOpacity: 0.5,
-        }}
-      />
+      <Polyline geometry={geometry} options={polylineOptions} />
       <FullscreenControl />
-      <ZoomControl
-        options={{
-          float: 'right',
-          size: 'auto',
-        }}
-      />
+      <ZoomControl options={zoomControlOptions} />
     </Map>
   </YMaps>
 );
