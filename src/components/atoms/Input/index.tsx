acd05a73682e9172072da81c4c95f5b4e54cd2c2
Mike Avdeev <avdeev2033@icloud.com>
2020-10-02 12:16:37 +0300

    refactor: delete styled-compoents, transfet styles to global css styles, wrap into memo, and delete unused props

--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -1,47 +1,17 @@
-import React, { FC, ChangeEvent, MutableRefObject } from 'react';
-import styled from 'styled-components';
-
-const StyledInput = styled.input<InputProps>`
-  width: 230px;
-  border: 2px;
-  font-size: 1.2rem;
-  border-radius: 0.2rem;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-style: solid;
-
-  border-color: #d3d3d4;
-  outline: none;
-
-  &:hover {
-    border-color: hsl(240, 1%, 72%);
-  }
-
-  &:focus {
-    outline: none;
-    border-color: #2d90e0;
-    box-shadow: 0 0 0 2px hsla(220, 90%, 56%, 0.2);
-  }
-`;
+import React, {
+  ChangeEvent,
+  FC,
+  memo,
+  MutableRefObject,
+} from 'react';
 
 export interface InputProps {
-  inputRef: MutableRefObject<HTMLInputElement | undefined>;
-  value: string;
+  ref: MutableRefObject<HTMLInputElement | null>;
   onChange: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Input: FC<InputProps> = ({
-  inputRef,
-  value,
-  onChange,
-}) => (
-  <StyledInput
-    inputRef={inputRef}
-    value={value}
-    onChange={onChange}
-  />
+const Input: FC<InputProps> = ({ ref, onChange }) => (
+  <input className="input-submit" ref={ref} onChange={onChange} />
 );
 
-export default Input;
+export default memo(Input);
