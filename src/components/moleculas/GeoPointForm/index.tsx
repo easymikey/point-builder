ad53657ae04bd920a93d8ae711d8e0e6bab9612f
Mike Avdeev <avdeev2033@icloud.com>
2020-10-02 13:23:05 +0300

    fix: fix warning bind with forward ref on GeoPointForm

--- src/components/moleculas/GeoPointForm/index.tsx
+++ src/components/moleculas/GeoPointForm/index.tsx
@@ -1,4 +1,8 @@
-import React, { FC, SyntheticEvent } from 'react';
+import React, {
+  forwardRef,
+  ForwardRefRenderFunction,
+  SyntheticEvent,
+} from 'react';
 import Button from '../../atoms/Button';
 import Input, { InputProps } from '../../atoms/Input';
 
@@ -6,15 +10,14 @@ interface GeoPointForm extends InputProps {
   onSubmit: (event: SyntheticEvent) => void;
 }
 
-const GeoPointForm: FC<GeoPointForm> = ({
-  ref,
-  onChange,
-  onSubmit,
-}) => (
+const GeoPointForm: ForwardRefRenderFunction<
+  HTMLInputElement,
+  GeoPointForm
+> = ({ onChange, onSubmit }, ref) => (
   <form className="geo-point-form" onSubmit={onSubmit}>
     <Input ref={ref} onChange={onChange} />
     <Button type="submit" buttonName="Добавить" onClick={onSubmit} />
   </form>
 );
 
-export default GeoPointForm;
+export default forwardRef(GeoPointForm);
