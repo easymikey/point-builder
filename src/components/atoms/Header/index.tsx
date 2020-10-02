78d7cba79c6be997d900683d6a062f85bd0cbac3
Mike Avdeev <avdeev2033@icloud.com>
2020-10-02 11:59:53 +0300

    refactor: delete styled-components from Header and transfet styled to global css styles

--- src/components/atoms/Header/index.tsx
+++ src/components/atoms/Header/index.tsx
@@ -1,28 +1,10 @@
-import React, { FC } from 'react';
-import styled from 'styled-components';
-
-const StyledHeader = styled.h1`
-  margin: 0;
-  height: 3rem;
-  width: 100%;
-  border-bottom-width: 1px;
-
-  border-bottom-color: rgba(199, 197, 197, 0.5);
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-bottom-style: solid;
-`;
-
-
-
+import React, { FC, memo } from 'react';
 interface HeaderProps {
   title: string;
 }
 
 const Header: FC<HeaderProps> = ({ title }) => (
-  <StyledHeader>{title}</StyledHeader>
+  <h1>{title}</h1>
 );
 
-export default Header;
+export default memo(Header);
