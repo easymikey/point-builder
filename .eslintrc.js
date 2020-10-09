4aa65421f43f1b8d08218814c755d8c7e9c04e83
Mike Avdeev <avdeev2033@icloud.com>
2020-10-09 12:11:21 +0300

    refactor: change eslint confign file (.eslintrc to .eslintrc.js)

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  plugins: [
+    'react',
+    'react-app',
+    'react-hooks',
+    '@typescript-eslint'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+    mocha: true
+  },
+  rules: {
+    'react/prop-types': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'no-multi-assign': 'off',
+    'no-use-before-define': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'object-curly-newline': 'warn',
+    'dot-notation': 'warn',
+    'import/prefer-default-export': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+  }
+};
